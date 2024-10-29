2d6178286548b08826bf7c71f4f2f28084a1625e
Sebastian Koszuta <sebastian.koszuta@gmail.com>
2024-10-29 12:58:13 +0100

    [UI] Global transaction state - Implemented a global transaction state to allow it to be changed outside of the transaction component - Bumped biome to 1.9.4

--- ui/src/context/ChatProvider.tsx
+++ ui/src/context/ChatProvider.tsx
@@ -1,12 +1,48 @@
+import type { AssetId, ChainId } from "@/api";
 import { useChat } from "ai/react";
-import type { PropsWithChildren } from "react";
+import { type PropsWithChildren, useEffect } from "react";
 import { ChatCtx } from "./chatCtx";
+import { TransactionActionType, TransactionType, useTransaction } from "./transactionCtx";
+
+type ChatTransaction = {
+  kind: TransactionType.Teleport;
+  source: ChainId;
+  destination: ChainId;
+  asset: AssetId;
+  amount: number;
+};
 
 export const ChatProvider: React.FC<PropsWithChildren> = ({ children }) => {
   const chat = useChat({
     api: import.meta.env.VITE_CHAT_ENDPOINT,
     keepLastMessageOnError: true,
   });
+
+  const { data, setData } = chat;
+  const { dispatch } = useTransaction();
+
+  useEffect(() => {
+    if (data?.[0]) {
+      const [chatTransaction] = data as [ChatTransaction];
+
+      if (chatTransaction.kind === TransactionType.Teleport) {
+        dispatch({
+          type: TransactionActionType.INITIATE_TRANSACTION,
+          payload: {
+            type: chatTransaction.kind,
+            formData: {
+              from: chatTransaction.source,
+              to: chatTransaction.destination,
+              asset: chatTransaction.asset,
+              amount: chatTransaction.amount,
+            },
+          },
+        });
+      }
+
+      setData(undefined);
+    }
+  }, [data, setData, dispatch]);
 
   return <ChatCtx.Provider value={chat}>{children}</ChatCtx.Provider>;
 };
